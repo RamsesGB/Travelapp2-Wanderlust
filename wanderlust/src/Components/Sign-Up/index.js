38e4bf4ffeab249b17dbbba647d0c72a207281cf
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-01 18:02:07 -0400

    deleted guide sign up route in an attempt to create a more dynamic sign up component that works for both types of users. began to create input form for sign up component

--- wanderlust/src/Components/Sign-Up/index.js
+++ wanderlust/src/Components/Sign-Up/index.js
@@ -3,18 +3,46 @@ import "../../App.css";
 import "./SignUp.css";
 // import signUpIcon from "../../Images/teddy-kelley-105544-unsplash.jpg";
 
-
 function SignUp() {
   return (
-      <section className="SignUp-Container">
-        <aside className="SignUp-image-box">
-          {/* <img alt="Lady on hike" src={signUpIcon}/> */}
-        </aside>
-        <div>
-          <h2>Sign Up</h2>
-
-        </div>
-      </section>
+    <section className="SignUp-Container">
+      <aside className="SignUp-image-box">{/* Image Container */}</aside>
+      <div>
+        <h2>Sign Up</h2>
+        <form>
+          <label>
+            <input
+              id="fnameInput"
+              maxLength="20"
+              placeholder="First Name"
+              name="fname"
+              type="text"
+            ></input>
+            <input
+              id="lnameInput"
+              maxLength="20"
+              placeholder="Last Name"
+              name="lname"
+              type="text"
+            ></input>
+            <input
+              id="emailInput"
+              maxLength="20"
+              placeholder="Email"
+              name="Email"
+              type="email"
+            ></input>
+            <input
+              id="passwordInput"
+              minLength="6"
+              placeholder="Password"
+              name="password"
+              type="password"
+            ></input>
+          </label>
+        </form>
+      </div>
+    </section>
   );
 }
 
