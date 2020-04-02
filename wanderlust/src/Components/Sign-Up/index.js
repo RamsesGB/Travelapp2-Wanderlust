f00d1d8c8d904dc76ff95ac141ff2dbf50355381
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-01 20:03:46 -0400

    added onChange handlers to sign up form, added axios dependency to project

--- wanderlust/src/Components/Sign-Up/index.js
+++ wanderlust/src/Components/Sign-Up/index.js
@@ -1,45 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "../../App.css";
 import "./SignUp.css";
-// import signUpIcon from "../../Images/teddy-kelley-105544-unsplash.jpg";
+
 
 function SignUp() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <section className="SignUp-Container">
       <aside className="SignUp-image-box">{/* Image Container */}</aside>
-      <div>
+      <div className="Form-Container">
         <h2>Sign Up</h2>
-        <form>
-          <label>
-            <input
-              id="fnameInput"
-              maxLength="20"
-              placeholder="First Name"
-              name="fname"
-              type="text"
-            ></input>
-            <input
-              id="lnameInput"
-              maxLength="20"
-              placeholder="Last Name"
-              name="lname"
-              type="text"
-            ></input>
-            <input
-              id="emailInput"
-              maxLength="20"
-              placeholder="Email"
-              name="Email"
-              type="email"
-            ></input>
-            <input
-              id="passwordInput"
-              minLength="6"
-              placeholder="Password"
-              name="password"
-              type="password"
-            ></input>
-          </label>
+        <form
+        // ADD CORRECT ENDPOINT ONCE SERVER IS RUNNING!
+          onSubmit={event => {
+            event.preventDefault();
+            axios.post("/", { fname, lname, email, password });
+          }}
+        >
+          <label htmlFor="fnameInput">First Name</label>
+          <input
+            id="fnameInput"
+            maxLength="20"
+            name="fname"
+            type="text"
+            onChange={event => {
+              setFname(event.target.value);
+            }}
+          ></input>
+
+          <label htmlFor="lnameInput">Last Name</label>
+          <input
+            id="lnameInput"
+            maxLength="20"
+            name="lname"
+            type="text"
+            onChange={event => {
+              setLname(event.target.value);
+            }}
+          ></input>
+
+          <label htmlFor="emailInput">Email</label>
+          <input
+            id="emailInput"
+            maxLength="20"
+            name="Email"
+            type="email"
+            onChange={event => {
+              setEmail(event.target.value);
+            }}
+          ></input>
+
+          <label htmlFor="passwordInput">Password</label>
+          <input
+            id="passwordInput"
+            minLength="6"
+            name="password"
+            type="password"
+            onChange={event => {
+              setPassword(event.target.value);
+            }}
+          ></input>
+          <input className="form-btn" type="submit" />
         </form>
       </div>
     </section>
