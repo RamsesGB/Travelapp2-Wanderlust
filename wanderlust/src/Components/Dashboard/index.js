ed820fea44bb7cc2cd507ecd27bf2adefdab194d
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-04-07 21:29:52 -0400

    creating dashboard navigation

--- wanderlust/src/Components/Dashboard/index.js
+++ wanderlust/src/Components/Dashboard/index.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Switch, Route, NavLink, Link } from "react-router-dom";
+import { Switch, Route, NavLink, useRouteMatch } from "react-router-dom";
 // import "./App.css";
 
 function Dashboard() {
+  let match = useRouteMatch();
+
   return (
     <div className="App">
       {/* ---------------- > Dashboard links START */}
+      <ul className="Nav-Container">
+        <li>
+          <NavLink to={`${match.url}/Profile`}>Profile</NavLink>
+        </li>
+        <li>
+          <NavLink to={`${match.url}/AddExperiences`}>Add Experiences</NavLink>
+        </li>
+        <li>
+          <NavLink to={`${match.url}/ManageExperiences`}>
+            Manage Experiences
+          </NavLink>
+        </li>
+      </ul>
 
       {/* --------------- > Dashboard links END */}
 
